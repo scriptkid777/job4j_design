6040d0d5b49878c9e125263c40affd51fca1c5ee
scriptkid777 <44677406+scriptkid777@users.noreply.github.com>
2022-04-20 00:03:11 +0700

    5.2.2. Реализовать Store<T extends Base> [#157]

---
+++ src/test/java/ru/job4j/generics/RoleStoreTest.java
@@ -0,0 +1,32 @@
+package ru.job4j.generics;
+
+import org.junit.Assert;
+import org.junit.Test;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertThat;
+
+public class RoleStoreTest {
+    @Test
+    public void whenReplaceRole() {
+        RoleStore roleStore = new RoleStore();
+        Role one = new Role("Admin");
+        Role two = new Role("User");
+        Role three = new Role("Manager");
+        roleStore.add(one);
+        roleStore.add(two);
+        roleStore.replace("Admin", three);
+        assertThat(roleStore.findById("User") , is(two));
+        assertThat(roleStore.findById("Admin"), is(three));
+    }
+
+    @Test
+    public void whenDeleteUserThenUserIsNull() {
+        RoleStore roleStore = new RoleStore();
+        roleStore.add(new Role("netuser"));
+        roleStore.delete("netuser");
+        Role result = roleStore.findById("netuser");
+        assertNull(result);
+    }
+
+}
