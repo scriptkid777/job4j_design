5f3bec1a0f4ba63b23d9c14792f7117b3e282bac
scriptkid777 <44677406+scriptkid777@users.noreply.github.com>
2022-05-15 13:10:44 +0700

    1. Динамический список на массиве. [#158]

--- src/main/java/ru/job4j/collection/SimpleArrayList.java
+++ src/main/java/ru/job4j/collection/SimpleArrayList.java
@@ -78,7 +78,7 @@ public class SimpleArrayList<T> implements List<T> {
     }
     private T[] chSize() {
         if (container.length == 0) {
-         throw new NullPointerException("Длина контейнера равна 0");
+         container[0] = null;
         }
         if (size == container.length) {
             container = Arrays.copyOf(container, container.length * 2);
