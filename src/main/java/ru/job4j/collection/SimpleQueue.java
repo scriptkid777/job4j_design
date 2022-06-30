1216697b5268a6145fd03c2e1b8eebb55afb2b32
scriptkid777 <44677406+scriptkid777@users.noreply.github.com>
2022-06-30 23:31:40 +0700

    5. Очередь на двух стеках [#refactoring]

--- src/main/java/ru/job4j/collection/SimpleQueue.java
+++ src/main/java/ru/job4j/collection/SimpleQueue.java
@@ -1,10 +1,15 @@
 package ru.job4j.collection;
 
+import java.util.NoSuchElementException;
+
 public class SimpleQueue<T> {
     private final SimpleStack<T> in = new SimpleStack<>();
     private final SimpleStack<T> out = new SimpleStack<>();
 
     public T poll() {
+        if (out.isEmpty() && in.isEmpty()) {
+            throw new NoSuchElementException("оба стека пусты");
+        }
         if (out.isEmpty()) {
             while (!in.isEmpty()) {
                 out.push(in.pop());
