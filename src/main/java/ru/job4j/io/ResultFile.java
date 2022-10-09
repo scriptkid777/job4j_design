5c612c972dda89943abe9e6861c145de8f24c58e
scriptkid777 <44677406+scriptkid777@users.noreply.github.com>
2022-10-09 21:26:56 +0700

    0.1. FileOutputStream. [#252488]

---
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -0,0 +1,20 @@
+package ru.job4j.io;
+
+import java.io.FileOutputStream;
+
+public class ResultFile {
+    public static void main(String[] args) {
+        try (FileOutputStream out = new FileOutputStream("result.txt")) {
+            out.write("Таблица  умножения распечатана".getBytes());
+            out.write(System.lineSeparator().getBytes());
+            for (byte i = 0; i < 10; i++) {
+                for (byte j = 0; j < 10; j++) {
+                    out.write((((i + 1) * (j + 1)) + System.lineSeparator()).getBytes());
+                }
+            }
+            out.write(System.lineSeparator().getBytes());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
