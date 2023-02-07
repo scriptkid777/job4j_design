0f907aefec1655a020df69f61ba570d915a79d03
scriptkid777 <44677406+scriptkid777@users.noreply.github.com>
2023-02-07 23:09:58 +0700

    исправление

--- src/main/java/ru/job4j/io/Analysis.java
+++ src/main/java/ru/job4j/io/Analysis.java
@@ -6,25 +6,28 @@ public class Analysis {
     public static void unavailable(String source, String target) {
         var flag = true;
         try (PrintWriter out = new PrintWriter(new BufferedOutputStream(new FileOutputStream(target)))) {
-            BufferedReader reader = new BufferedReader(new FileReader(source));
-for (String line = reader.readLine(); line != null; line = reader.readLine()) {
-    String[] array = line.split(" ");
-    if (flag && (line.contains("400") || line.contains("500 "))) {
-        flag = false;
-       out.append(array[1]).append(";");
-    } else if (!flag && (line.contains("200") || line.contains("300"))) {
-         flag = true;
-        out.append(array[1]).append(";").append(System.lineSeparator());
-    }
-}
-        }   catch (IOException e) {
+          try (BufferedReader reader = new BufferedReader(new FileReader(source))) {
+            for (String line = reader.readLine(); line != null; line = reader.readLine()) {
+                String[] array = line.split(" ");
+                if (flag && (line.contains("400") || line.contains("500 "))) {
+                    flag = false;
+                    out.append(array[1]).append(";");
+                } else if (!flag && (line.contains("200") || line.contains("300"))) {
+                    flag = true;
+                    out.append(array[1]).append(";").append(System.lineSeparator());
+                }
+            }
+            } catch (IOException e) {
+              e.printStackTrace();
+          }
+        } catch (IOException e) {
             e.printStackTrace();
         }
+
     }
+        public static void main(String[]args) {
+            new Analysis().unavailable("server.log", "unavailable.csv");
+        }
 
 
-
-    public static void main(String[] args) {
-         new Analysis().unavailable("server.log", "unavailable.csv");
     }
-}
