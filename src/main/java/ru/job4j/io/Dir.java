5d83fa48492ca9fa44ef98d8b4d9a6e0457051aa
scriptkid777 <44677406+scriptkid777@users.noreply.github.com>
2023-03-09 22:47:45 +0700

    9.0. Файлы и директории. Класс File [#исправлено]

--- src/main/java/ru/job4j/io/Dir.java
+++ src/main/java/ru/job4j/io/Dir.java
@@ -13,7 +13,7 @@ public class Dir {
         }
         System.out.println(String.format("size : %s ", file.getTotalSpace()));
         for (File subfile : file.listFiles()) {
-            System.out.println(subfile.getName() + " : " + subfile.length()  + " kilobytes");
+            System.out.println(String.format("%s: %d kilobytes ", subfile.getName(), subfile.length()));
         }
     }
 
