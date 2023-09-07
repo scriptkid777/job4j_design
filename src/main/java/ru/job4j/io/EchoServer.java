400b2398670c9a607899132404a861a03048e5d1
scriptkid777 <servsystem@yandex.ru>
2023-09-07 15:33:20 +0700

    3. Бот [#7921]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -6,17 +6,20 @@ import java.net.Socket;
 public class EchoServer {
     public static void main(String[] args) throws IOException {
         try (ServerSocket server = new ServerSocket(9000)) {
-            if (!server.isClosed()) {
+            while (!server.isClosed()) {
                Socket socket = server.accept();
                try (OutputStream out = socket.getOutputStream()) {
                    BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                    out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
                   String str = in.readLine();
-                       System.out.println(str);
-                        if (str.contains("msg=Bye")) {
-                            System.out.println("BYE BYE");
-                            server.close();
-                       }
+                  String output = "What";
+                  if (str.contains("msg=Hello")) {
+                      output = "Hello";
+                       } else if (str.contains("msg=Exit")) {
+                      output = "Server closed";
+                      server.close();
+                  }
+                  out.write((output + "\n").getBytes());
                    out.flush();
                }
             }
