4a81eaf9cdd27ea75fa5a1b6393f0e2d2e64721b
scriptkid777 <servsystem@yandex.ru>
2023-09-09 23:47:19 +0700

    1. Что такое Сериализация? [#313163]

--- src/main/java/ru/job4j/serialization/Contact.java
+++ src/main/java/ru/job4j/serialization/Contact.java
@@ -23,10 +23,8 @@ public class Contact implements Serializable {
 
     @Override
     public String toString() {
-        return "Contact{" +
-                "zipCode=" + zipCode +
-                ", phone='" + phone + '\'' +
-                '}';
+        return "Contact{"
+                + "zipCode=" + zipCode + ", phone='" + phone + '\'' + '}';
     }
 
     public static void main(String[] args) throws IOException, ClassNotFoundException {
