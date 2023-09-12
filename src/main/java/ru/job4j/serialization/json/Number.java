5541ce7f20bc8add5b537c771827fc76ba892ce0
scriptkid777 <servsystem@yandex.ru>
2023-09-12 10:37:41 +0700

    2. Формат JSON [#313164]

---
+++ src/main/java/ru/job4j/serialization/json/Number.java
@@ -0,0 +1,13 @@
+package ru.job4j.serialization.json;
+
+public class Number {
+    private final String number;
+
+    public Number(String number) {
+        this.number = number;
+    }
+    @Override
+    public String toString() {
+        return "{" + '\"' + number + '\"' + '}';
+    }
+}
