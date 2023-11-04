a629ab3e6a3c8e1797ceecf889ec704ecbc582d9
scriptkid777 <servsystem@yandex.ru>
2023-11-05 02:39:37 +0700

    0. Select [#6861]

---
+++ db/select/insert.sql
@@ -0,0 +1,5 @@
+insert into fauna (name, avg_age, discovery_date) values ('swordfish', 60, '1901-09-01');
+insert into fauna (name, avg_age, discovery_date) values ('bluefish', 25000, '05.03.1890');
+insert into fauna(name, avg_age, discovery_date) values('dog', 20000, '11.06.1915');
+insert into fauna(name, avg_age, discovery_date) values('cat', 15000, '19.05.1923');
+
