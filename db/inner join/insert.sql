68928668260c0672de8c7746a83b6dffd26ada4a
scriptkid777 <servsystem@yandex.ru>
2023-11-07 18:16:08 +0700

    1. Inner join [#373617]

--- db/inner join/insert.sql
+++
@@ -1,9 +0,0 @@
-insert into snils (number) values(12743224177);
-insert into snils (number) values(19872871588);
-insert into snils (number) values(41982781199);
-insert into snils (number) values(54518291766);
-
-insert into people(name, surname, people_id) values ('Igor','Vasiliev',1);
-insert into people(name, surname, people_id) values ('Oleg','Kropotkin',2);
-insert into people(name, surname, people_id) values ('Vladimir','Petrov',3);
-insert into people(name, surname, people_id) values ('Vlada','Ivanova',4);
