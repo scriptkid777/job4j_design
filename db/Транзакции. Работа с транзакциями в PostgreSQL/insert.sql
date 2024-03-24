ddfc2537ccb114c2d67cf0f5a44483702b283328
scriptkid777 <servsystem@yandex.ru>
2024-03-24 15:33:12 +0700

    4.4. Транзакции. Работа с транзакциями в PostgreSQL [#504813]

---
+++ db/Транзакции. Работа с транзакциями в PostgreSQL/insert.sql
@@ -0,0 +1,3 @@
+insert into products (name, producer, count, price) VALUES ('product_1', 'producer_1', 3, 50);
+insert into products (name, producer, count, price) VALUES ('product_2', 'producer_2', 15, 100);
+insert into products (name, producer, count, price) VALUES ('product_3', 'producer_3', 20, 115);
